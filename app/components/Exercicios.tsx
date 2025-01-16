28dddbd1d5bdff3284f6dfd566374ebf12c6040e
YuriKauan10 <yurrikauan@gmail.com>
2025-01-16 13:21:06 -0300

    chore: navegação pra pag fatorial e botão adicionado

--- app/components/Exercicios.tsx
+++ app/components/Exercicios.tsx
@@ -2,10 +2,16 @@ import Link from "next/link"
 
 export default function Exercicios(){
     return (
-        <div>
+        <div className="ex">
+            <div>
             <Link href = './alfabeto'>
                 <button className="alfabeto">Alfabeto</button>
             </Link>
+            </div>
+            <Link href = './fatorial'>
+                <button className="fatorial">Fatorial</button>
+            </Link>
+            
         </div>
     )
 }
---
+++ app/fatorial/page.tsx
@@ -0,0 +1,10 @@
+import Topo from "../components/Topo"
+
+export default function Fatorial(){
+    return (
+        <div>
+            <Topo/>
+            Fatorial
+        </div>
+    )
+}
